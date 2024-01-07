449584bfbd2526f939c546a21810703873dd8eb8
Jiya Solanki <jiya.solanki@cubexo.io>
2024-01-07 17:15:14 +0530

    Day 1: Added a script that dynamically updates the greetings

---
+++ Day-1/greetings.js
@@ -0,0 +1,21 @@
+function updateGreeting() 
+{
+    const currTime=new Date();
+    const currHour=currTime.getHours();
+    let greeting;
+    if(currHour<12) 
+    {
+        greeting='Good Morning!';
+    } 
+    else if(currHour<18) 
+    {
+        greeting='Good Afternoon!';
+    } 
+    else 
+    {
+        greeting='Good Evening!';
+    }
+  
+    document.getElementById('greeting').textContent=greeting;
+}
+updateGreeting();
